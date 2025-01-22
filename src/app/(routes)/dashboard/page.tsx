736e48984cc0032d20a7d6561622fb4fa0c794c5
SeiamAlMahmud <samahmud49@gmail.com>
2025-01-22 22:19:55 +0600

    add logout button

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import Header from '@/app/_components/Header'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Header />
+      Dashboard
+      </div>
   )
 }
 
--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,15 +1,19 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const Header = () => {
+const Header = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  console.log(user);
   return (
     <div>
       <header className="bg-black">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
           <Link className=" text-teal-600 flex items-center flex-nowrap gap-4" href="/">
-            <Image
-              src={"/logo.png"} width={50} height={50} alt="logo" />
+            <Image src={"/logo.png"} width={50} height={50} alt="logo" />
             <span className=" text-white font-extrabold text-xl  hover:text-gray-500/75">Home</span>
           </Link>
 
@@ -19,23 +23,18 @@ const Header = () => {
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> About </a>
                 </li>
-
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> Careers </a>
                 </li>
-
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> History </a>
                 </li>
-
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> Services </a>
                 </li>
-
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> Projects </a>
                 </li>
-
                 <li>
                   <a className="text-white transition hover:text-gray-500/75" href="#"> Blog </a>
                 </li>
@@ -44,27 +43,29 @@ const Header = () => {
 
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
-                <LoginLink>
-                  <div
-                    className="block rounded-md bg-black-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-700"
-                  >
-                    Login
-                  </div>
-                </LoginLink>
-
-                <RegisterLink>
-
-                  <div
-                    className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:bg-gray-800 hover:text-white sm:block hover:border-purple-500 hover:border-[1px]"
-                  >
-                    Register
-                  </div>
-                </RegisterLink>
+                {user ? (
+                  <LogoutLink>
+                    <div className="block rounded-md bg-black-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-700">
+                      Logout
+                    </div>
+                  </LogoutLink>
+                ) : (
+                  <>
+                    <LoginLink>
+                      <div className="block rounded-md bg-black-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-700">
+                        Login
+                      </div>
+                    </LoginLink>
+                    <RegisterLink>
+                      <div className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-black transition hover:bg-gray-800 hover:text-white sm:block hover:border-purple-500 hover:border-[1px]">
+                        Register
+                      </div>
+                    </RegisterLink>
+                  </>
+                )}
               </div>
 
-              <button
-                className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
-              >
+              <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
                 <span className="sr-only">Toggle menu</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -81,9 +82,8 @@ const Header = () => {
           </div>
         </div>
       </header>
-
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
