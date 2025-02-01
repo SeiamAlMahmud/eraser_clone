3bd778b81644a42510b65202d983c4e560369edf
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-01 14:29:18 +0600

    convex create user function created

--- convex/user.tsx
+++ convex/user.tsx
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { query } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 export const getUser = query({
   args: {
@@ -14,3 +14,25 @@ export const getUser = query({
     return result;
   },
 });
+
+export const createUser = mutation({
+    args: {
+      name: v.string(),
+      email: v.string(),
+      image: v.string(),
+    },
+    handler: async (ctx, args) => {
+      const existingUser = await ctx.db
+        .query("user")
+        .filter((q) => q.eq(q.field("email"), args.email))
+        .first();
+  
+      if (existingUser) {
+        return { success: false, message: "Email already exists!" };
+      }
+  
+      const newUser = await ctx.db.insert("user", args);
+      return { success: true, user: newUser };
+    },
+  });
+  
--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useQuery } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
 import { useEffect } from 'react';
 import { api } from '../../../../convex/_generated/api';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
@@ -9,8 +9,19 @@ const Dashboard = () => {
   const { user }: any = useKindeBrowserClient();
   const getUser = useQuery(api.user.getUser, { email: user?.email });
 
+  const createUser = useMutation(api.user.createUser);
+
   useEffect(() => {
     if(user){
+      if (getUser == undefined) {
+        createUser({
+          name: user.given_name + ' ' + user.family_name,
+          email: user.email,
+          image: user.picture
+        }).then(res => {
+          console.log(res,"res")
+        })
+      }
       console.log(getUser)
     }
   }, [user]);
