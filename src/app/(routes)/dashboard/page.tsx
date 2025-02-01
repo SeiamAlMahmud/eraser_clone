56d952ccc1fee960a83fd4da8d9cfa770d84a11a
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-01 14:41:19 +0600

    additional check , if already exist in convex or not

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,30 +1,36 @@
 'use client';
 
-import { useMutation, useQuery } from 'convex/react';
+import { useConvex, useMutation, useQuery } from 'convex/react';
 import { useEffect } from 'react';
 import { api } from '../../../../convex/_generated/api';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 
 const Dashboard = () => {
+  const convex = useConvex();
   const { user }: any = useKindeBrowserClient();
-  const getUser = useQuery(api.user.getUser, { email: user?.email });
+  // const getUser = useQuery(api.user.getUser, { email: user?.email });
 
   const createUser = useMutation(api.user.createUser);
 
   useEffect(() => {
-    if(user){
-      if (getUser == undefined) {
-        createUser({
-          name: user.given_name + ' ' + user.family_name,
-          email: user.email,
-          image: user.picture
-        }).then(res => {
-          console.log(res,"res")
-        })
-      }
-      console.log(getUser)
+    if (user) {
+      checkUser();
     }
   }, [user]);
+  
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser,{ email: user?.email });
+    // console.log(result, "65654");
+    if (!result?.length) {
+      createUser({
+        name: user.given_name + ' ' + user.family_name,
+        email: user.email,
+        image: user.picture,
+      }).then((res) => {
+        // console.log(res,"res")
+      });
+    }
+  }
   return <div>page</div>;
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Hero />
     </div>
   );
