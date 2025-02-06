e1cf987eece27b8ddc4c6d959cca53fbb22ed5b5
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-06 16:14:34 +0600

    in filed api,  i added archive, document, whiteboard

--- convex/files.tsx
+++ convex/files.tsx
@@ -6,6 +6,9 @@ export const createFile = mutation({
     fileName: v.string(),
     teamId: v.string(),
     createdBy: v.string(),
+    archive: v.optional(v.boolean()),
+    document: v.optional(v.string()),
+    whiteboard: v.optional(v.string()),
   },
   handler: async (ctx, args_0) => {
     const result = await ctx.db.insert('files', args_0);
