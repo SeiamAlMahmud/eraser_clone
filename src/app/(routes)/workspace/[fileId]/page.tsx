e07fc2d5dca4e79a910edf7225c331ee0b6fc9cd
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-07 02:14:39 +0600

    workspace boil template added

--- src/app/(routes)/workspace/[fileId]/page.tsx
+++ src/app/(routes)/workspace/[fileId]/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
 import WorkSpaceHeader from '../_components/WorkSpaceHeader';
 
 const WorkSpace = () => {
   return (
     <div>
-        <WorkSpaceHeader />
+      <WorkSpaceHeader />
+
+      {/* Workspace Layout  */}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* Document  */}
+        <div className='h-screen'>a</div>
+        {/* Canvas */}
+        <div className='h-screen'>b</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default WorkSpace;
