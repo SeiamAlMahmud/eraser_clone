a98560030885bf887495c7b1e358fad4a4402981
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-08 02:55:46 +0600

    image and checklist remove

--- src/app/(routes)/workspace/_components/Editor.tsx
+++ src/app/(routes)/workspace/_components/Editor.tsx
@@ -1,11 +1,10 @@
 'use client';
+
 import React, { useEffect, useRef, useState } from 'react';
 import { BlockToolConstructable } from '@editorjs/editorjs';
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import EditorjsList from '@editorjs/list';
-// import Checklist from '@editorjs/checklist'
-// import ImageTool from '@editorjs/image';
 import CodeBox from '@bomdi/codebox';
 import DragDrop from 'editorjs-drag-drop';
 import Undo from 'editorjs-undo';
