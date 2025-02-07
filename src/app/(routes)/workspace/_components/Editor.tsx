6a7354f041c60c94ce33e1e71fdcb0567aa55f85
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-08 02:54:39 +0600

    remive image and checklist

--- src/app/(routes)/workspace/_components/Editor.tsx
+++ src/app/(routes)/workspace/_components/Editor.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useEffect, useRef, useState } from 'react';
 import { BlockToolConstructable } from '@editorjs/editorjs';
-// @ts-nocheck
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import EditorjsList from '@editorjs/list';
