521bf3f5d81bd134adee929aa39d5198ce5384f3
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-07 17:27:13 +0600

    use // @ts-nocheck for stop check for this page

--- src/app/(routes)/workspace/_components/Editor.tsx
+++ src/app/(routes)/workspace/_components/Editor.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useEffect, useRef } from 'react';
 import { BlockToolConstructable } from '@editorjs/editorjs';
+// @ts-nocheck
 import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header';
 import EditorjsList from '@editorjs/list';
